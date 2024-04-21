28058003cdf8b68140c43381e67711a910752445
Sakshi <sakshikumari.raj.csrl@gmail.com>
2024-04-21 22:51:10 +0530

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,29 @@
 import React, { Fragment } from 'react';
-import {Table} from 'react-bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Table} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Employees from './Employees';
+import {Link,useNavigate} from 'react-router-dom';
+
 
 function Home(){
+    
+    let history = useNavigate();
+    
+    const handleEdit = (id, name, age) =>{
+        localStorage.setItem('Name',name);
+        localStorage.setItem('Age',age);
+        localStorage.setItem('Id',id);
+    }
+
+    const handleDelete = (id) => {
+        var index = Employees.map(function(e){
+            return e.id
+        }).indexOf(id);
+
+        Employees.splice(index,1);
+
+        history('/'); 
+    }
     return(
     <Fragment>
      <div style={{margin:"10rem"}}>
@@ -15,6 +35,9 @@ function Home(){
                     </th>
                     <th>
                         Age
+                    </th>
+                    <th>
+                        Action
                     </th>
                 </tr>
             </thead>
@@ -31,14 +54,26 @@ function Home(){
                                 <td>
                                     {item.Age}
                                 </td>
+                                <td>
+                                    <Link to = {'/edit'}>
+                                    <Button onClick={() => handleEdit(item.id, item.Name, item.Age)}>EDIT</Button>
+                                    </Link>
+                                    &nbsp;
+                                    <Button onClick={() => handleDelete(item.id)}>DELETE</Button>
+                                </td>
                             </tr>
-                        )
+                    )
                     })
                     :
                     "No data available"
                 }
             </tbody>
         </Table>
+        <br>
+        </br>
+        <Link className='d-grid gap-2' to = "/create">
+            <Button size = "lg">Create</Button>
+        </Link>
     </div> 
     </Fragment>
 )  
