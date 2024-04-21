c12f2950f85ba3c2c2dd34b8b3e92373874626b3
Sakshi <sakshikumari.raj.csrl@gmail.com>
2024-04-21 19:47:58 +0530

    first commit

---
+++ src/components/Employees.js
@@ -0,0 +1,19 @@
+const Employees = [
+    {
+        id:"1",
+        Name:"Sohan Deshmukh",
+        Age:"39"
+    },
+    { 
+        id:"2",
+        Name:"Preeti Pandey",
+        Age:"32"
+    },
+    { 
+        id:"3",
+        Name:"Aishwarya Chouhan",
+        Age:"43"
+    }
+]
+
+export default Employees;
